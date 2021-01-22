724ce18d84ffbf20f34687e90072c581884c384a
try-1 <try-1c@mai.ru>
2021-01-22 20:27:07 +0300

    correct work of MultyThreading

--- lesson_3/FolderAnalyzerLib/src/main/java/ru/pivovarov/files/FolderProcessor.java
+++ lesson_3/FolderAnalyzerLib/src/main/java/ru/pivovarov/files/FolderProcessor.java
@@ -1,6 +1,5 @@
 package ru.pivovarov.files;
-
-	import java.io.File;
+  import java.io.File;
     import java.util.ArrayList;
     import java.util.HashMap;
     import java.util.List;
@@ -8,14 +7,19 @@ package ru.pivovarov.files;
 
 public class FolderProcessor {
 
+
+
     public Map<String, List<FileInformation>> getInformation(List<String> folderNames) {
+
+        List<Thread> threads = new ArrayList<>();
 
         Map<String, List<FileInformation>> folders = new HashMap<>();
 
         for (String folderName : folderNames) {
-
-            // для каждого folderName отдельный поток
+          
             Runnable task = () -> {
+
+                System.out.println("In Task " + folderName);
 
                 List<FileInformation> fileInformations = new ArrayList<>();
 
@@ -26,23 +30,34 @@ public class FolderProcessor {
                 String fileNames[] = folder.list();
 
                 for (int i = 0; i < files.length; i++) {
-                fileInformations.add(new FileInformation(fileNames[i], files[i].length()));
+                    fileInformations.add(new FileInformation(fileNames[i], files[i].length()));
                 }
+               
                 folders.put(folderName, fileInformations);
+                System.out.println("From task " + folderName);
             };
-            Thread t1 = new Thread(task);
-            t1.start();
-            System.out.println("*********** THREAD "+ t1.getName()+ " GET STARTED");
+
+            Thread thread = new Thread(task);
+            thread.start();
+            threads.add(thread);
+           
+        }
+
+        for (Thread thread1 : threads) {
+
             try {
-                t1.join();
+                thread1.join(); 
             } 
             catch (InterruptedException e) {
-                 throw new IllegalArgumentException(e);
+                throw new IllegalArgumentException (e);
             }
+
         }
         return folders;
     }
 }
+
+
 
 
 
